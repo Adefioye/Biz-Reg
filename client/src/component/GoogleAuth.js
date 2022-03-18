587318884562d659d97bd51a90be69696f7c4bc1
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-03-18 02:46:09 +0100

    implemented google oauth using hooks and wired to button on header

---
+++ client/src/component/GoogleAuth.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { signIn, signOut } from "../actions";
+import Button from "@material-ui/core/Button";
+
+function GoogleAuth() {
+  const [auth, setAuth] = useState(null);
+
+  const dispatch = useDispatch();
+  const isSignedIn = useSelector((state) => state.auth.isSignedIn);
+
+  // const handleAuthChange = () =>
+  //   useCallback(
+  //     (isSignedIn) => {
+  //       if (isSignedIn) {
+  //         dispatch(signIn());
+  //       } else {
+  //         dispatch(signOut());
+  //       }
+  //     },
+  //     [dispatch]
+  //   );
+
+  useEffect(() => {
+    function handleAuthChange(isSignedIn) {
+      if (isSignedIn) {
+        dispatch(signIn());
+      } else {
+        dispatch(signOut());
+      }
+    }
+
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "683580204463-4nfhg92lvh1hjefhsjqja8fp8n28a8q8.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          setAuth(window.gapi.auth2.getAuthInstance());
+          handleAuthChange(
+            window.gapi.auth2.getAuthInstance().isSignedIn.get()
+          );
+          window.gapi.auth2
+            .getAuthInstance()
+            .isSignedIn.listen(handleAuthChange);
+        });
+    });
+  }, [dispatch]);
+
+  const handleSignIn = () => {
+    auth.signIn();
+  };
+
+  const handleSignOut = () => {
+    auth.signOut();
+  };
+
+  const renderAuthButton = () => {
+    if (isSignedIn === null) {
+      return null;
+    } else if (isSignedIn) {
+      return (
+        <Button variant="contained" onClick={handleSignOut}>
+          Sign Out
+        </Button>
+      );
+    } else {
+      return (
+        <Button variant="contained" onClick={handleSignIn}>
+          Sign In
+        </Button>
+      );
+    }
+  };
+
+  return <>{renderAuthButton()}</>;
+}
+
+export default GoogleAuth;
---
+++ client/src/component/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+import GoogleAuth from "./GoogleAuth";
+
+function Header() {
+  return (
+    <AppBar>
+      <Toolbar>
+        <Grid container justifyContent="space-between" direction="row">
+          <Grid item>
+            <Typography>BizReg</Typography>
+          </Grid>
+          <Grid item>
+            <Typography>Search</Typography>
+          </Grid>
+          <Grid item>
+            <GoogleAuth />
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
